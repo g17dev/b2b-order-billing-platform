cc6e5701ce9507305e83135bde9b94b5cd065da8
g17dev <89885702+g17dev@users.noreply.github.com>
2026-01-31 17:04:59 -0700

    chore: new structure to the project

---
+++ src/main/java/org/portfolio/Main.java
@@ -0,0 +1,7 @@
+package org.portfolio;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/portfolio/domain/order/Order.java
@@ -0,0 +1,4 @@
+package org.portfolio.domain.order;
+
+public class Order {
+}
